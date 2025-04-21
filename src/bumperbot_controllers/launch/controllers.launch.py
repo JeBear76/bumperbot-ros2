e90a0e039c121140b95f08a066a0f4813062c12c
Jebear76 <jerepondumie@gmail.com>
2025-04-21 13:58:49 +0200

    tmp

--- src/bumperbot_controllers/launch/controllers.launch.py
+++ src/bumperbot_controllers/launch/controllers.launch.py
@@ -6,39 +6,46 @@ from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 from launch.conditions import UnlessCondition, IfCondition
+from launch.actions import GroupAction
 import os
 
+
 def generate_launch_description():
+    use_simple_controller_args = DeclareLaunchArgument(
+        name="use_simple_controller",
+        default_value="True",
+        description="Use my custom simple controller or not",
+    )
+
     use_python_args = DeclareLaunchArgument(
-        name="use_python",
-        default_value='False',
-        description='Use python or not'
-        )
-    
+        name="use_python", 
+        default_value="False", 
+        description="Use python or not"
+    )
+
     wheel_radius_args = DeclareLaunchArgument(
-        name="wheel_radius",
-        default_value='0.033',
-        description='Wheel radius'
-        )
-    
+        name="wheel_radius", 
+        default_value="0.033", 
+        description="Wheel radius"
+    )
+
     wheel_separation_args = DeclareLaunchArgument(
-        name="wheel_separation",
-        default_value='0.17',
-        description='Wheel separation'
-        )
-    
+        name="wheel_separation", 
+        default_value="0.17", 
+        description="Wheel separation"
+    )
+
     is_sim_args = DeclareLaunchArgument(
         name="is_sim",
-        default_value='True',
-        description='Are we working in the simulation'
-        )
+        default_value="True",
+        description="Are we working in the simulation",
+    )
 
     port_args = DeclareLaunchArgument(
-        name="port",
-        description="COM Port for arduino",
-        default_value="/dev/ttyACM0"
+        name="port", description="COM Port for arduino", default_value="/dev/ttyACM0"
     )
-    
+
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
     use_python = LaunchConfiguration("use_python")
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
@@ -52,10 +59,11 @@ def generate_launch_description():
             [
                 "xacro ",
                 os.path.join(
-                    bumperbot_description_dir, "urdf", "bumperbot.urdf.xacro"
-                ),
+                    bumperbot_description_dir, 
+                    "urdf", 
+                    "bumperbot.urdf.xacro"),
                 " is_sim:=", is_sim,
-                " port:=", port
+                " port:=", port,
             ]
         ),
         value_type=str,
@@ -65,7 +73,7 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}],
-        condition=UnlessCondition(is_sim)
+        condition=UnlessCondition(is_sim),
     )
 
     controller_manager = Node(
@@ -73,16 +81,16 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[
             {
-                "robot_description": robot_description,
+                "robot_description": robot_description, 
                 "use_sim_time": is_sim
             },
             os.path.join(
                 get_package_share_directory("bumperbot_controllers"),
                 "config",
                 "bumperbot_controllers.yaml",
-            )            
+            ),
         ],
-        condition=UnlessCondition(is_sim)
+        condition=UnlessCondition(is_sim),
     )
 
     joint_state_broadcaster_spawnwer = Node(
@@ -95,38 +103,46 @@ def generate_launch_description():
         ],
     )
 
-    simple_velocity_controller_spawnwer = Node(
+    bumperbot_diffdrive_controller_spawnwer = Node(
         package="controller_manager",
         executable="spawner",
         arguments=[
-            "simple_velocity_controller", 
-            "--controller-manager", 
-            "/controller_manager"
+            "bumperbot_controller",
+            "--controller-manager",
+            "/controller_manager",
         ],
+        condition=UnlessCondition(use_simple_controller),
     )
 
-    simple_controller_cpp = Node(
-        package="bumperbot_controllers",
-        executable="simple_controller",
-        parameters=[
-            {
-                "wheel_radius": wheel_radius,
-                "wheel_separation": wheel_separation
-            }
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "simple_velocity_controller",
+                    "--controller-manager",
+                    "/controller_manager",
+                ],
+            ),
+            Node(
+                package="bumperbot_controllers",
+                executable="simple_controller",
+                parameters=[
+                    {"wheel_radius": wheel_radius, "wheel_separation": wheel_separation}
+                ],
+                condition=UnlessCondition(use_python),
+            ),
+            Node(
+                package="bumperbot_controllers",
+                executable="simple_controller.py",
+                parameters=[
+                    {"wheel_radius": wheel_radius, "wheel_separation": wheel_separation}
+                ],
+                condition=IfCondition(use_python),
+            ),
         ],
-        condition=UnlessCondition(use_python)
-    )
-
-    simple_controller_python = Node(
-        package="bumperbot_controllers",
-        executable="simple_controller.py",
-        parameters=[
-            {
-                "wheel_radius": wheel_radius,
-                "wheel_separation": wheel_separation
-            }
-        ],
-        condition=IfCondition(use_python)
     )
 
     return LaunchDescription(
@@ -136,12 +152,11 @@ def generate_launch_description():
             wheel_separation_args,
             is_sim_args,
             port_args,
+            use_simple_controller_args,
             robot_state_publisher,
             controller_manager,
             joint_state_broadcaster_spawnwer,
-            simple_velocity_controller_spawnwer,
-            simple_controller_cpp,
-            simple_controller_python,
-
+            bumperbot_diffdrive_controller_spawnwer,
+            simple_controller,
         ]
     )
---
+++ src/bumperbot_controllers/launch/keyboard_teleop.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    teleop_node = Node(
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        name='keyboard_teleop',
+        parameters=[
+            os.path.join(get_package_share_directory("bumperbot_controllers"), "config", "keyboard_config.yaml")
+        ],
+        remappings=[
+            ('cmd_vel', '/bumperbot_controller/cmd_vel')
+        ]
+
+    )
+
+    return LaunchDescription([
+        teleop_node
+    ])
