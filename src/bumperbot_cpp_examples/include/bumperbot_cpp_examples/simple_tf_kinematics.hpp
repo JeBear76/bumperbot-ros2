0355d73d9a59f0cc1acc59288df7363aebf9f947
Jebear76 <jerepondumie@gmail.com>
2025-04-24 11:55:17 +0200

    simple tf publisher cpp

--- src/bumperbot_cpp_examples/include/bumperbot_cpp_examples/simple_tf_kinematics.hpp
+++ src/bumperbot_cpp_examples/include/bumperbot_cpp_examples/simple_tf_kinematics.hpp
@@ -2,15 +2,23 @@
 #define SIMPLE_TF_KINEMATICS
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 
 class SimpleTfKinematics : public rclcpp::Node
 {
 public:
-    SimpleTfKinematics(const std::string &node_name = "simple_tf_kinematics");
+    SimpleTfKinematics(const std::string &node_name = "simple_tf_kinematics");    
 
 private:
     std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster_;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> dynamic_tf_broadcaster_;
     geometry_msgs::msg::TransformStamped static_transform_stamped_;
+    geometry_msgs::msg::TransformStamped dynamic_transform_stamped_;
+    rclcpp::TimerBase::SharedPtr dynamic_tf_timer_;
+
+    void publishDynamicTransform();
+    double x_increment_;
+    double last_x_;
 };
 #endif // SIMPLE_TF_KINEMATICS
--- src/bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
+++ src/bumperbot_cpp_examples/src/simple_tf_kinematics.cpp
@@ -1,8 +1,11 @@
 #include <bumperbot_cpp_examples/simple_tf_kinematics.hpp>
 
-SimpleTfKinematics::SimpleTfKinematics(const std::string &node_name) : Node(node_name)
+using namespace std::chrono_literals;
+
+SimpleTfKinematics::SimpleTfKinematics(const std::string &node_name) : Node(node_name), x_increment_(0.01), last_x_(0.0)
 {
     static_tf_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    dynamic_tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
 
     static_transform_stamped_.header.frame_id = "bumperbot_base";
     static_transform_stamped_.child_frame_id = "bumperbot_top";        
@@ -18,6 +21,28 @@ SimpleTfKinematics::SimpleTfKinematics(const std::string &node_name) : Node(node
     static_tf_broadcaster_->sendTransform(static_transform_stamped_);
 
     RCLCPP_INFO(this->get_logger(), "Static transform from base_link to base_footprint published.");
+
+    dynamic_tf_timer_ = create_wall_timer(
+        0.1s,
+        std::bind(&SimpleTfKinematics::publishDynamicTransform, this));
+}
+
+void SimpleTfKinematics::publishDynamicTransform()
+{
+    dynamic_transform_stamped_.header.frame_id = "odom";
+    dynamic_transform_stamped_.child_frame_id = "bumperbot_base";
+    dynamic_transform_stamped_.header.stamp = get_clock()->now();
+    dynamic_transform_stamped_.transform.translation.x = last_x_ + x_increment_;
+    dynamic_transform_stamped_.transform.translation.y = 0.0;
+    dynamic_transform_stamped_.transform.translation.z = 0.0;
+    dynamic_transform_stamped_.transform.rotation.x = 0.0;
+    dynamic_transform_stamped_.transform.rotation.y = 0.0;
+    dynamic_transform_stamped_.transform.rotation.z = 0.0;
+    dynamic_transform_stamped_.transform.rotation.w = 1.0;
+
+    dynamic_tf_broadcaster_->sendTransform(dynamic_transform_stamped_);
+    
+    last_x_ += x_increment_;
 }
 
 int main(int argc, char **argv)
